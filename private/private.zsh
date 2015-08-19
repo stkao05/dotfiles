d28655e58394dfa355db376abaade52b6c284ca9
stkao05 <st.kao.05@gmail.com>
2015-08-19 18:57:51 +0800

    Update private

--- private/private.zsh
+++
@@ -1,8 +0,0 @@
-# Stash your private environment setup in ~/.private.zsh. This means they'll stay out
-# of your main dotfiles repository (which may be public, like this one), but
-# you'll have access to them in your scripts.
-if [[ -a ~/.private_rc.zsh ]]
-then
-  source ~/.private_rc.zsh
-fi
-
